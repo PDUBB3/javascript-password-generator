83dd0d4b4f54bbd7136f15e9a381f9198fb7c997
Pamela Bhopal <pamela_bhopal@hotmail.co.uk>
2021-03-20 02:22:35 +0000

    create generate passworth function and length with prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const generatePassword = function () {
+  let passwordlength = prompt("What is the length of your password?");
+  console.log(passwordlength);
+
+  if (passwordlength > 8 && passwordlength < 128) {
+    alert("Hurray");
+  }
+};
+
+// Declare function which generates password
+function generatePassword() {
+  // create variable to store generated password
+  var password = "";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
