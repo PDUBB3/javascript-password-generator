5bddc601eee3f3dda7c61df6bf7a8cd6c4bb8eb3
Pamela Bhopal <pamela_bhopal@hotmail.co.uk>
2021-03-20 02:29:55 +0000

    add lowercase array

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declare lower case character arrays in global memory
+const lowercasearray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Declare upper case character arrays in global memory
+
+// Declare numeric character arrays in global memory
+
+// Declare special character arrays in global memory
+
 const generatePassword = function () {
   let passwordlength = prompt("What is the length of your password?");
   console.log(passwordlength);
@@ -14,6 +50,9 @@ const generatePassword = function () {
 function generatePassword() {
   // create variable to store generated password
   var password = "";
+
+  // return with password thats created
+  return password;
 }
 
 // Write password to the #password input
