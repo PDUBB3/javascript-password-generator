a5d139f25fb262c004557030517825bad1ae94ec
Pamela Bhopal <pamela_bhopal@hotmail.co.uk>
2021-03-21 15:50:07 +0000

    declare lower case arrays in global memory

---
+++ Develop/script.js
@@ -0,0 +1,43 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Declare lower case character arrays in global memory
+const lowercasearray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
